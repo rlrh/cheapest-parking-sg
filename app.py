ef6482c05cb4248e453e24c5d93c01b01cf46bcc
Rayner Lim <rlrh1996@gmail.com>
2018-07-20 16:38:58 +0000

    bug fixes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import date, time, datetime, timedelta
 from geopy.distance import vincenty
 from copy import deepcopy
 from helpers import *
+from flask_mail import Mail, Message
 
 data_file = "data-lta-orchard-minified.json"
 data_schema_file = "data-schema.json"
@@ -12,11 +13,21 @@ data_schema_file = "data-schema.json"
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'carparkssg@gmail.com'
+app.config['MAIL_PASSWORD'] = '$tGabriel6'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 with app.open_resource(data_file) as f:
     data = json.load(f)
     data_length = len(data)
 with app.open_resource(data_schema_file) as schema_file:
     schema = json.load(schema_file)
+
+# WEBSITE ROUTES
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -55,8 +66,6 @@ def index():
             messages.append("Error ocurred while searching for carparks")
             messages.append(str(e))
 
-        #markers = list(map(lambda carpark: carpark[schema["location"]], cheapest_carparks))
-
         search_params = {"start": from_datetime.strftime("%a, %-d %b %Y, %-I:%M %p"), "end": to_datetime.strftime("%a, %-d %b %Y, %-I:%M %p"), "center": center_loc, "radius": radius, "place":request.form["place"]}
 
         if not messages:
@@ -69,73 +78,12 @@ def index():
         if carpark_id != -1 and carpark_id < data_length:
             center = data[carpark_id][schema["location"]]
             name = data[carpark_id][schema["name"]]
-            return render_template("index2.html", markers=markers, schema=schema, center=center, name=name)
+            return render_template("index.html", markers=markers, schema=schema, center=center, name=name)
         else:
-            return render_template("index2.html", markers=markers, schema=schema)
+            return render_template("index.html", markers=markers, schema=schema)
 
-@app.route('/browse')
-def browse():
-    carpark_id = int(request.args.get('id', -1))
-    if carpark_id != -1 and carpark_id < data_length:
-        return render_template("browse.html", data=add_carparks_availability(data, schema), schema=schema, id=carpark_id, name=data[carpark_id][schema["name"]])
-    else:
-        return render_template("browse.html", data=add_carparks_availability(data, schema), schema=schema)
-
-@app.route('/terms')
-def terms():
-    return render_template("terms.html")
-
-@app.route('/api/all')
-def api_all():
-    try:
-        return jsonify(add_carparks_availability(data, schema))
-    except:
-        return abort(400)
-
-@app.route('/api/single')
-def api_single():
-    try:
-        carpark_id = int(request.args["id"])
-        #
-        return jsonify(data[carpark_id], schema)
-    except Exception as e:
-        return str(e)
-
-@app.route('/api/singleprice')
-def api_single_price():
-    messages = []
-    try:
-        from_datetime = datetime.strptime(request.args["start"], "%Y-%m-%dT%H:%M")
-    except Exception as e:
-        messages.append("Start datetime invalid. " + str(e))
-    try:
-        to_datetime = datetime.strptime(request.args["end"], "%Y-%m-%dT%H:%M")
-    except Exception as e:
-        messages.append("End datetime invalid. " + str(e))
-    try:
-        carpark_id = int(request.args["id"])
-    except Exception as e:
-        messages.append("Carpark ID invalid. " + str(e))
-    try:
-        priceonly = bool(request.args.get("priceonly", 0))
-    except Exception as e:
-        messages.append("Price only invalid. " + str(e))
-    try:
-        carpark = deepcopy(data[carpark_id])
-        charges = carpark_charges(carpark, from_datetime, to_datetime, schema)
-    except Exception as e:
-        messages.append("Error occurred. " + str(e))
-    if messages:
-        return jsonify(messages)
-    else:
-        if priceonly:
-            return jsonify(charges)
-        else:
-            carpark[schema["price"]] = charges
-            return jsonify(carpark)
-
-@app.route('/api/multiple')
-def api_multiple():
+@app.route('/cheapest')
+def cheapest():
     messages = []
     try:
         from_datetime = datetime.strptime(request.args["start"], "%Y-%m-%dT%H:%M")
@@ -157,17 +105,22 @@ def api_multiple():
     except Exception as e:
         messages.append("Radius invalid. " + str(e))
     try:
+        place = request.args["place"]
+    except Exception as e:
+        messages.append("Radius invalid. " + str(e))
+    try:
         cheapest_carparks = cheapest_carparks_within_radius(data, center_loc, radius, from_datetime, to_datetime, schema)
     except Exception as e:
         messages.append("Error ocurred while searching for carparks. " + str(e))
 
-    if not messages:
-        return jsonify(cheapest_carparks)
-    else:
-        abort(400)
-        # return jsonify(messages)
+    search_params = {"start": from_datetime.strftime("%a, %-d %B %Y, %-I:%M %p"), "end": to_datetime.strftime("%a, %-d %B %Y, %-I:%M %p"), "center": center_loc, "radius": radius, "place":place}
 
-@app.route('/fuzzy', methods=["GET", "POST"])
+    if not messages:
+        return render_template("results.html", search_params=search_params, results=cheapest_carparks, schema=schema)
+    else:
+        return render_template("apology.html", messages=messages)
+
+@app.route('/multi', methods=["GET", "POST"])
 def fuzzy():
     if request.method == "POST":
         messages = []
@@ -221,23 +174,89 @@ def fuzzy():
         markers = list(map(lambda carpark: carpark[schema["location"]], data))
         return render_template("fuzzy.html", markers=markers, schema=schema)
 
-@app.route('/calc')
-def calculator():
-    return render_template("calculator.html", data=add_carparks_availability(data, schema), schema=schema)
-
-@app.route('/browse2')
-def browse2():
+@app.route('/browse')
+def browse():
     carpark_id = int(request.args.get('id', -1))
     markers = list(map(lambda carpark: carpark[schema["location"]], data))
     add_carparks_availability(data, schema)
     if carpark_id != -1 and carpark_id < data_length:
         center = data[carpark_id][schema["location"]]
-        return render_template("browse2.html", markers=markers, id=carpark_id, center=center, name=data[carpark_id][schema["name"]])
+        return render_template("browse_ajax.html", markers=markers, id=carpark_id, center=center, name=data[carpark_id][schema["name"]])
     else:
-        return render_template("browse2.html", markers=markers)
+        return render_template("browse_ajax.html", markers=markers)
 
-@app.route('/cheapest')
-def cheapest():
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        try:
+            msg = Message(request.form.get("subject","No subject"), sender = 'carparkssg@gmail.com', recipients = ['carparkssg@gmail.com'])
+            msg.body = "From: " + request.form.get("email","No email") + "\n\n" + request.form.get("message","No message")
+            mail.send(msg)
+            return render_template("about.html", sent=1)
+        except:
+            return render_template("about.html", sent=0)
+    else:
+        if "place" in request.args:
+            return render_template("about.html", place=request.args["place"])
+        else:
+            return render_template("about.html")
+
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
+
+# API ROUTES
+
+@app.route('/api/all')
+def api_all():
+    try:
+        return jsonify(add_carparks_availability(data, schema))
+    except:
+        return abort(400)
+
+@app.route('/api/single')
+def api_single():
+    try:
+        carpark_id = int(request.args["id"])
+        return jsonify(data[carpark_id], schema)
+    except Exception as e:
+        return str(e)
+
+@app.route('/api/singleprice')
+def api_single_price():
+    messages = []
+    try:
+        from_datetime = datetime.strptime(request.args["start"], "%Y-%m-%dT%H:%M")
+    except Exception as e:
+        messages.append("Start datetime invalid. " + str(e))
+    try:
+        to_datetime = datetime.strptime(request.args["end"], "%Y-%m-%dT%H:%M")
+    except Exception as e:
+        messages.append("End datetime invalid. " + str(e))
+    try:
+        carpark_id = int(request.args["id"])
+    except Exception as e:
+        messages.append("Carpark ID invalid. " + str(e))
+    try:
+        priceonly = bool(request.args.get("priceonly", 0))
+    except Exception as e:
+        messages.append("Price only invalid. " + str(e))
+    try:
+        carpark = deepcopy(data[carpark_id])
+        charges = carpark_charges(carpark, from_datetime, to_datetime, schema)
+    except Exception as e:
+        messages.append("Error occurred. " + str(e))
+    if messages:
+        return jsonify(messages)
+    else:
+        if priceonly:
+            return jsonify(charges)
+        else:
+            carpark[schema["price"]] = charges
+            return jsonify(carpark)
+
+@app.route('/api/multipleprice')
+def api_multiple():
     messages = []
     try:
         from_datetime = datetime.strptime(request.args["start"], "%Y-%m-%dT%H:%M")
@@ -259,27 +278,12 @@ def cheapest():
     except Exception as e:
         messages.append("Radius invalid. " + str(e))
     try:
-        place = request.args["place"]
-    except Exception as e:
-        messages.append("Radius invalid. " + str(e))
-    try:
         cheapest_carparks = cheapest_carparks_within_radius(data, center_loc, radius, from_datetime, to_datetime, schema)
     except Exception as e:
         messages.append("Error ocurred while searching for carparks. " + str(e))
 
-    search_params = {"start": from_datetime.strftime("%a, %-d %B %Y, %-I:%M %p"), "end": to_datetime.strftime("%a, %-d %B %Y, %-I:%M %p"), "center": center_loc, "radius": radius, "place":place}
-
     if not messages:
-        return render_template("results.html", search_params=search_params, results=cheapest_carparks, schema=schema)
+        return jsonify(cheapest_carparks)
     else:
-        return render_template("apology.html", messages=messages)
-
-@app.route('/contact', methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        return render_template("about.html", sent=1)
-    else:
-        if "place" in request.args:
-            return render_template("about.html", place=request.args["place"])
-        else:
-            return render_template("about.html")
+        abort(400)
+        # return jsonify(messages)
